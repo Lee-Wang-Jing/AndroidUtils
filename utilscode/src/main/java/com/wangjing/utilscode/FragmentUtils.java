6ae1ef7c9b396dcd65877fd9bea8c5f2ca6e26ef
WangJing <919335417@qq.com>
2022-06-13 22:18:09 +0800

    first commit

---
+++ utilscode/src/main/java/com/wangjing/utilscode/FragmentUtils.java
@@ -0,0 +1,234 @@
+package com.wangjing.utilscode;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.annotation.IdRes;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import java.util.Collections;
+import java.util.List;
+
+public class FragmentUtils {
+
+    private static final int TYPE_ADD_FRAGMENT = 0x01;
+    private static final int TYPE_SHOW_FRAGMENT = 0x01 << 1;
+    private static final int TYPE_HIDE_FRAGMENT = 0x01 << 2;
+    private static final int TYPE_SHOW_HIDE_FRAGMENT = 0x01 << 3;
+    private static final int TYPE_REPLACE_FRAGMENT = 0x01 << 4;
+    private static final int TYPE_REMOVE_FRAGMENT = 0x01 << 5;
+    private static final int TYPE_REMOVE_TO_FRAGMENT = 0x01 << 6;
+
+    private static final String ARGS_ID = "args_id";
+    private static final String ARGS_IS_HIDE = "args_is_hide";
+    private static final String ARGS_IS_ADD_STACK = "args_is_add_stack";
+    private static final String ARGS_TAG = "args_tag";
+
+
+    private FragmentUtils() {
+        throw new UnsupportedOperationException("u can't instantiate me...");
+    }
+
+    /**
+     * Add fragment.
+     *
+     * @param fm          The manager of fragment.
+     * @param add         The fragment will be add.
+     * @param containerId The id of container.
+     */
+    public static void add(@NonNull final FragmentManager fm,
+                           @NonNull final Fragment add,
+                           @IdRes final int containerId) {
+        add(fm, add, containerId, null, false, false);
+    }
+
+
+    /**
+     * Add fragment.
+     *
+     * @param fm          The manager of fragment.
+     * @param add         The fragment will be add.
+     * @param containerId The id of container.
+     * @param tag         The tag of fragment.
+     * @param isHide      True to hide, false otherwise.
+     * @param isAddStack  True to add fragment in stack, false otherwise.
+     */
+    public static void add(@NonNull final FragmentManager fm,
+                           @NonNull final Fragment add,
+                           @IdRes final int containerId,
+                           final String tag,
+                           final boolean isHide,
+                           final boolean isAddStack) {
+        putArgs(add, new Args(containerId, tag, isHide, isAddStack));
+        operateNoAnim(TYPE_ADD_FRAGMENT, fm, null, add);
+    }
+
+    /**
+     * Show fragment.
+     *
+     * @param show The fragment will be show.
+     */
+    public static void show(@NonNull final Fragment show) {
+        putArgs(show, false);
+        operateNoAnim(TYPE_SHOW_FRAGMENT, show.getParentFragmentManager(), null, show);
+    }
+
+    /**
+     * Hide fragment.
+     *
+     * @param hide The fragment will be hide.
+     */
+    public static void hide(@NonNull final Fragment hide) {
+        putArgs(hide, true);
+        operateNoAnim(TYPE_HIDE_FRAGMENT, hide.getParentFragmentManager(), null, hide);
+    }
+
+    /**
+     * Show fragment then hide other fragment.
+     *
+     * @param show The fragment will be show.
+     * @param hide The fragment will be hide.
+     */
+    public static void showHide(@NonNull final Fragment show,
+                                @NonNull final Fragment hide) {
+        showHide(show, Collections.singletonList(hide));
+    }
+
+    /**
+     * Show fragment then hide other fragment.
+     *
+     * @param show The fragment will be show.
+     * @param hide The fragment will be hide.
+     */
+    public static void showHide(@NonNull final Fragment show, @NonNull final List<Fragment> hide) {
+        for (Fragment fragment : hide) {
+            putArgs(fragment, fragment != show);
+        }
+        operateNoAnim(TYPE_SHOW_HIDE_FRAGMENT, show.getParentFragmentManager(), show, hide.toArray(new Fragment[0]));
+    }
+
+    private static void putArgs(final Fragment fragment, final boolean isHide) {
+        Bundle bundle = fragment.getArguments();
+        if (bundle == null) {
+            bundle = new Bundle();
+            fragment.setArguments(bundle);
+        }
+        bundle.putBoolean(ARGS_IS_HIDE, isHide);
+    }
+
+    private static void putArgs(final Fragment fragment, final Args args) {
+        Bundle bundle = fragment.getArguments();
+        if (bundle == null) {
+            bundle = new Bundle();
+            fragment.setArguments(bundle);
+        }
+        bundle.putInt(ARGS_ID, args.id);
+        bundle.putBoolean(ARGS_IS_HIDE, args.isHide);
+        bundle.putBoolean(ARGS_IS_ADD_STACK, args.isAddStack);
+        bundle.putString(ARGS_TAG, args.tag);
+    }
+
+    private static void operateNoAnim(final int type, @Nullable final FragmentManager fm,
+                                      final Fragment src,
+                                      Fragment... dest) {
+        if (fm == null) return;
+        FragmentTransaction ft = fm.beginTransaction();
+        operate(type, fm, ft, src, dest);
+    }
+
+    private static void operate(final int type,
+                                @NonNull final FragmentManager fm,
+                                final FragmentTransaction ft,
+                                final Fragment src,
+                                final Fragment... dest) {
+        if (src != null && src.isRemoving()) {
+            Log.e("FragmentUtils", src.getClass().getName() + " is isRemoving");
+            return;
+        }
+        String name;
+        Bundle args;
+        switch (type) {
+            case TYPE_ADD_FRAGMENT:
+                for (Fragment fragment : dest) {
+                    args = fragment.getArguments();
+                    if (args == null) return;
+                    name = args.getString(ARGS_TAG, fragment.getClass().getName());
+                    Fragment fragmentByTag = fm.findFragmentByTag(name);
+                    if (fragmentByTag != null && fragmentByTag.isAdded()) {
+                        ft.remove(fragmentByTag);
+                    }
+                    ft.add(args.getInt(ARGS_ID), fragment, name);
+                    if (args.getBoolean(ARGS_IS_HIDE)) ft.hide(fragment);
+                    if (args.getBoolean(ARGS_IS_ADD_STACK)) ft.addToBackStack(name);
+                }
+                break;
+            case TYPE_HIDE_FRAGMENT:
+                for (Fragment fragment : dest) {
+                    ft.hide(fragment);
+                }
+                break;
+            case TYPE_SHOW_FRAGMENT:
+                for (Fragment fragment : dest) {
+                    ft.show(fragment);
+                }
+                break;
+            case TYPE_SHOW_HIDE_FRAGMENT:
+                ft.show(src);
+                for (Fragment fragment : dest) {
+                    if (fragment != src) {
+                        ft.hide(fragment);
+                    }
+                }
+                break;
+            case TYPE_REPLACE_FRAGMENT:
+                args = dest[0].getArguments();
+                if (args == null) return;
+                name = args.getString(ARGS_TAG, dest[0].getClass().getName());
+                ft.replace(args.getInt(ARGS_ID), dest[0], name);
+                if (args.getBoolean(ARGS_IS_ADD_STACK)) ft.addToBackStack(name);
+                break;
+            case TYPE_REMOVE_FRAGMENT:
+                for (Fragment fragment : dest) {
+                    if (fragment != src) {
+                        ft.remove(fragment);
+                    }
+                }
+                break;
+            case TYPE_REMOVE_TO_FRAGMENT:
+                for (int i = dest.length - 1; i >= 0; --i) {
+                    Fragment fragment = dest[i];
+                    if (fragment == dest[0]) {
+                        if (src != null) ft.remove(fragment);
+                        break;
+                    }
+                    ft.remove(fragment);
+                }
+                break;
+        }
+        ft.commitAllowingStateLoss();
+        fm.executePendingTransactions();
+    }
+
+    private static class Args {
+        final int id;
+        final boolean isHide;
+        final boolean isAddStack;
+        final String tag;
+
+        Args(final int id, final boolean isHide, final boolean isAddStack) {
+            this(id, null, isHide, isAddStack);
+        }
+
+        Args(final int id, final String tag,
+             final boolean isHide, final boolean isAddStack) {
+            this.id = id;
+            this.tag = tag;
+            this.isHide = isHide;
+            this.isAddStack = isAddStack;
+        }
+    }
+}
