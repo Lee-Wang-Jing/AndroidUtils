f4a907204cd981986c9453aa2f1c13d1249b34d2
WangJing <919335417@qq.com>
2022-10-06 11:17:17 +0800

    first commit

--- utilscode/src/main/java/com/wangjing/utilscode/ImageLoaderUtils.java
+++ utilscode/src/main/java/com/wangjing/utilscode/ImageLoaderUtils.java
@@ -1,6 +1,7 @@
 package com.wangjing.utilscode;
 
 import android.content.Context;
+import android.graphics.drawable.Drawable;
 import android.widget.ImageView;
 
 import androidx.fragment.app.Fragment;
@@ -15,4 +16,14 @@ public class ImageLoaderUtils {
     public static void loadImage(Fragment fragment, ImageView imageView, String url) {
         Glide.with(fragment).load(url).into(imageView);
     }
+
+
+    public static void loadImage(Context context, ImageView imageView, Drawable drawable) {
+        Glide.with(context).load(drawable).into(imageView);
+    }
+
+    public static void loadImage(Fragment fragment, ImageView imageView, Drawable drawable) {
+        Glide.with(fragment).load(drawable).into(imageView);
+    }
+
 }
